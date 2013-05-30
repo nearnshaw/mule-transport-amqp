2759d1fe6188cdcaf04f135a259a2f0b047d3501
Gustavo Alberola <alberolagustavo@gmail.com>
2013-05-30 17:48:47 -0300

    Minor fixes (amqp.xml and Activator) Changed assembly generation for zip

--- amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/src/org/mule/tooling/ui/contribution/amqp/Activator.java
+++ amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/src/org/mule/tooling/ui/contribution/amqp/Activator.java
@@ -1,4 +1,4 @@
-package org.mule.tooling.ui.contribution.hl7;
+package org.mule.tooling.ui.contribution.amqp;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
