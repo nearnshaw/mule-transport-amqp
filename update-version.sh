840bbdd888bbf9a5fa804d5e8784334b7c5fdd38
Alejandro Sequeira <asequeira@gmail.com>
2014-10-21 13:14:56 -0300

    Create script for version update

---
+++ update-version.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z $4 ]; then
+    echo "Usage: \033[1mupdate-version.sh\033[0m MAVEN_VERSION_FROM MAVEN_VERSION_TO OSGI_VERSION_FROM OSGI_VERSION_TO";
+    exit 1
+fi
+mvn versions:set versions:commit -DnewVersion=$2
+sed -e "s/mule-transport-amqp-$1.jar/mule-transport-amqp-$2.jar/g" -i '' amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/build.properties
+sed -e "s/mule-transport-amqp-$1.zip/mule-transport-amqp-$2.zip/g" -i '' amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/build.properties
+find . -name MANIFEST.MF -exec sed -e "s/Bundle-Version:.*/Bundle-Version: $4/g" -i '' {} \; 
+find . -name feature.xml -exec sed -e "s/version=.*$3/version=\"$4/g" -i '' {} \; 
