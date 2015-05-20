2cf8a264e4a691976ca526de781b4cb32b46739d
Alejandro Sequeira <asequeira@gmail.com>
2015-05-20 12:36:45 -0300

    Add settings.xml parameter to update-version.sh to use maven repo credentials

--- update-version.sh
+++ update-version.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ -z $4 ]; then
-    echo "Usage: \033[1mupdate-version.sh\033[0m MAVEN_VERSION_FROM MAVEN_VERSION_TO OSGI_VERSION_FROM OSGI_VERSION_TO";
+if [ -z $5 ]; then
+    echo "Usage: \033[1mupdate-version.sh\033[0m MAVEN_VERSION_FROM MAVEN_VERSION_TO OSGI_VERSION_FROM OSGI_VERSION_TO MAVEN_SETTINGS";
     exit 1
 fi
-echo update-version "$1 $2 $3 $4"
-mvn versions:set versions:commit -DnewVersion="$2"
+echo update-version "$1 $2 $3 $4 $5"
+mvn --settings $5 versions:set versions:commit -DnewVersion="$2"
 sed -e "s/mule-transport-amqp-$1.jar/mule-transport-amqp-$2.jar/g" -i '' amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/build.properties
 sed -e "s/mule-transport-amqp-$1.zip/mule-transport-amqp-$2.zip/g" -i '' amqp-eclipse-plugin/org.mule.tooling.ui.contribution.amqp/build.properties
 find . -name MANIFEST.MF -exec sed -e "s/Bundle-Version:.*/Bundle-Version: $4/g" -i '' {} \; 
